1d5ed2c988405a9dea7067807be81861d79ae144
Kris Chambers <kris.chambers@outlook.com>
2026-03-13 14:38:35 -0400

    Turn off haskell tools for now

--- lua/me/lazy/langs/haskell.lua
+++ lua/me/lazy/langs/haskell.lua
@@ -1,6 +1,6 @@
--- return {}
-return {
-  'mrcjkb/haskell-tools.nvim',
-  version = '^6',
-  lazy = false, -- This plugin is already lazy
-}
+return {}
+-- return {
+--   'mrcjkb/haskell-tools.nvim',
+--   version = '^6',
+--   lazy = false, -- This plugin is already lazy
+-- }
